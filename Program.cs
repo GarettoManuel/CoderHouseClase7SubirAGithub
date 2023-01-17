dd677b3c83dc02d4b312fbb8fc62636f1e801689
GarettoManuel <garettomanuel@gmail.com>
2023-01-17 20:47:31 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoderHouseClase7SubirAGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
